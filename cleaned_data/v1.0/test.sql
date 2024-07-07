ec6a3218297a43a186348b0427469c3d8566a3ca
Bryan Johns <73319490+johbry17@users.noreply.github.com>
2024-07-06 23:39:06 -0400

    organize directory, split large csv's, created v1.0 subfolder

---
+++ cleaned_data/v1.0/test.sql
@@ -0,0 +1,19 @@
+-- displays all info about a table's schema
+-- replace with appropriate table name
+SELECT
+    column_name,
+    data_type,
+    character_maximum_length,
+    is_nullable
+FROM
+    information_schema.columns
+WHERE
+    table_name = 'hosts'
+ORDER BY
+    ordinal_position;
+	
+-- The map's app.py query, with LEFT JOINs to make it function for now
+SELECT * FROM listings
+LEFT JOIN listing_description ON listings.listing_id = listing_description.id
+LEFT JOIN listing_reviews ON listings.listing_id = listing_reviews.listing_id
+LEFT JOIN hosts ON listings.listing_id = hosts.listing_id;
